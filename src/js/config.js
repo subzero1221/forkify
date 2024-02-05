5bd682406852abe2d7aacd0454d380cb4e5fe165
subzero1221 <bkelekhsaevi@gmail.com>
2024-02-06 01:11:54 +0400

    initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '0ce0950b-122c-44ba-8cf7-c8cc55177783';
+export const MODAL_CLOSE_SEC = 2.5;
